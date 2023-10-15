4499036d618b80e97285a407e41b526413d9cd19
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-10-15 13:13:31 -0300

    EstudiantesApplication .java

--- Java/Semana 02 /.java
+++ Java/Semana 02 /.java
@@ -1,1 +1,8 @@
+package utn.estudiantes;
 
+import ...
+
+@SpringBootApplication
+public class EstudiantesApplication {
+  public static void main (String [] args) {SpringApplication.run(EstudiantesApplication.class, args);}
+  
