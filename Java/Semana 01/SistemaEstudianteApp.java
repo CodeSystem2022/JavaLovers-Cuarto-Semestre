ad0bc3f5984d8d0351ba78e89bf64b1063899b2e
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-10-15 14:13:05 -0300

    Update and rename SistemaEstudianteAppjava to SistemaEstudianteApp.java

--- Java/Semana 01/SistemaEstudianteApp.java
+++ Java/Semana 01/SistemaEstudianteApp.java
@@ -36,4 +36,15 @@ public class SistemaEstudiantesApp{
     //Método para ejecutar las opciones, va a regresar un valor booleano, ya que es el que
     //puede modificar el valor de la varible salir, si es verdadero termina el ciclo while.
     private static boolean ejecutarOpciones(Scanner consola, EstudianteDAO estudianteDAO){
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir =false;
+        switch ((opcion) {
+        case 1 -> { //Listar estudiantes
+            System.out.println("Listado de Estudiantes...");
+            //no muestra la información, solo recupera la info y regresa una lista
+            var estudiantes = estudianteDAO.listarEstudiantes();//recibe el listado
+            //vamos a iterar cada objecto de tipo estudiante
+            estudiantes.forEach(System.out::println);//para imprimir la lista
+        }//FDin caso 1
+        case 2 -> {//Buscar estudiante por id
     
