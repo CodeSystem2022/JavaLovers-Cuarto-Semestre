7cf2b49cb61c5fe71f4c9649d98175f8742a4bf8
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-11-02 22:25:36 -0300

    Update 1.2-(parte 3)Trabajamos  con las colecciones.py

--- Python/Semana 3/1.2-(parte 3)Trabajamos  con las colecciones.py
+++ Python/Semana 3/1.2-(parte 3)Trabajamos  con las colecciones.py
@@ -1,1 +1,15 @@
+#Tipo de colecciones -> False para colecciones vacias
+#Tipo colecciones -> True para todas las demas
+#Lista
+valor = []
+resultado = bool(valor)
+print(f'valor de una lista vacia: {valor}, Resultado: {resultado}')
 
+valor = [2,3,4]
+resultado = bool(valor)
+print(f'valor de lista con elementos: {valor}, Resultado: {resultado}')
+
+#Tupla
+valor = ()
+resultado = bool(valor)
+print(f'valor de una tupla vacia: {valor}, Resultado: {resultado}')
