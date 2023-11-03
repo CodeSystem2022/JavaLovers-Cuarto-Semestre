cedc437bbae409b2765aa96a4074fc5bcb117e17
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-11-02 22:42:42 -0300

    Update 1.2-(parte 2)Utlizamos  el tipo str(string).py

--- Python/Semana 3/1.2-(parte 2)Utlizamos  el tipo str(string).py
+++ Python/Semana 3/1.2-(parte 2)Utlizamos  el tipo str(string).py
@@ -1,1 +1,9 @@
+#Tipo string -> False '',True demas valores
+valor = 0
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 'Hola'
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
 
