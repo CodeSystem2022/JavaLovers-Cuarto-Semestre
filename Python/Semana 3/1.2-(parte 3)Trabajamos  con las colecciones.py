e01fe4c70cc1074cf4fb173edb5f247157696191
alexanahuel <112024900+alexanahuel@users.noreply.github.com>
2023-11-02 22:34:22 -0300

    Update 1.2-(parte 3)Trabajamos  con las colecciones.py

--- Python/Semana 3/1.2-(parte 3)Trabajamos  con las colecciones.py
+++ Python/Semana 3/1.2-(parte 3)Trabajamos  con las colecciones.py
@@ -13,3 +13,18 @@ print(f'valor de lista con elementos: {valor}, Resultado: {resultado}')
 valor = ()
 resultado = bool(valor)
 print(f'valor de una tupla vacia: {valor}, Resultado: {resultado}')
+
+#Codigo realizado por Alexa Nahuel
+
+valor = (5,)
+resultado = bool(valor)
+print(f'valor de una tupla con elementos: {valor}, Resultado: {resultado}')
+
+#Diccionario
+valor = {}
+resultado = bool(valor)
+print(f'valor de un diccionario vacio: {valor}, Resultado: {resultado}')
+
+valor = {'Nombre':'Juan', 'Apellido':'Perez'}
+resultado = bool(valor)
+print(f'valor de un diccionario con elementos: {valor}, Resultado: {resultado}')
