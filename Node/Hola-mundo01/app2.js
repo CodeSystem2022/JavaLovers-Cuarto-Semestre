04db51e36326638e2c11a66065103538d59f00bc
Alexa Nahuel <112024900+alexanahuel@users.noreply.github.com>
2023-11-09 15:23:26 -0300

    Create app2.js

---
+++ Node/Hola-mundo01/app2.js
@@ -0,0 +1,6 @@
+const saludar = (nombre) =>{  //Funcion de flecha (arrow)
+    return `Saludos de ${nombre}`; // Usamos backticks Alt + ctrl + }
+                                         
+} //esto es igual a: 'Saludo'+ nombre;
+console.log(saludar('Alexa'));
+
