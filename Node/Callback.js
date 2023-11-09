2c7d9b8a3e998298077f19b5d5f341883c34e784
Alexa Nahuel <112024900+alexanahuel@users.noreply.github.com>
2023-11-09 14:47:32 -0300

    Create Callback.js

---
+++ Node/Callback.js
@@ -0,0 +1,26 @@
+function hola(nombre, miCallback){
+    setTimeout(function () { 
+        console.log('Hola '+nombre);
+        miCallback(nombre);
+    },1000); // 1000 ms
+    
+}
+           
+
+function adios(nombre, otroCallback){
+    setTimeout(function () {
+        console.log('Adios '+nombre);
+        otroCallback();
+    },1500);
+}
+console.log('Iniciando el proceso...');
+hola('carlos', function(nombre) {
+   adios(nombre, function(){
+       console.log('Terminando el proceso...');
+
+    })
+});
+//hola('carlos', function(){});
+//adios('Carlos', function(){});
+
+
