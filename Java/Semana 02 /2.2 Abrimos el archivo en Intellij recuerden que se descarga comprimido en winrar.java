71f300df91815755251c15c4b8df4b035ad2f29e
Alexa Nahuel <112024900+alexanahuel@users.noreply.github.com>
2023-11-09 16:18:08 -0300

    Create 2.2 Abrimos el archivo en Intellij recuerden que se descarga comprimido en winrar.java

---
+++ Java/Semana 02 /2.2 Abrimos el archivo en Intellij recuerden que se descarga comprimido en winrar.java
@@ -0,0 +1,9 @@
+package utn.estudiantes;
+
+import ...
+
+@SpringBootApplication
+public class EstudiantesApplication {
+  
+  public static void main (String [] args) {SpringApplication.run(EstudiantesApplication.class, args);}
+  
