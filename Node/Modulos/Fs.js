9168bb690f1be8706206f1cd9367b66cb96c7497
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-11-10 19:54:01 -0300

    Update Fs.js

--- Node/Modulos/Fs.js
+++ Node/Modulos/Fs.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+
+//Primero leemos el archivo.txt
+function leer(ruta, cb){
+    fs.readFile(ruta, (err, data) => {
+        cb(data.toString());
+    });
+}
+
+
+//Segundo escribimos en el archivo.txt
+function escribir(ruta, contenido){
+    fs.writeFile(ruta, contenido, function(err) {
+        if (err) {
+            console.log("No se pudo escribir", err);
+        } else {
+            console.log("Se ha escrito correctamente");
+        }
+     });
+}
+
+// Tercero eliminamos el archivo1.txt
+function borrar(ruta, cb){
+    fs.unlink(ruta, cb); //elimina de manera asincrona
+}
+
+borrar(`${__dirname}/archivo1.txt`, console.log);
+
+//escribir(`${__dirname}/archivo1.txt`, 'Reescribimos el archivo', console.log);
+//leer(`${__dirname}/archivo.txt`, console.log); //Sintaxis ES6
 
