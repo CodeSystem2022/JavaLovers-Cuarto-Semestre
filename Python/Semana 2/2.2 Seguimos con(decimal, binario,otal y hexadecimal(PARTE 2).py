01db9a45fe46e89045bef06721038c53d5441e13
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-11-10 20:13:08 -0300

    Update 2.2 Seguimos con(decimal, binario,otal y hexadecimal(PARTE 2).py

--- Python/Semana 2/2.2 Seguimos con(decimal, binario,otal y hexadecimal(PARTE 2).py
+++ Python/Semana 2/2.2 Seguimos con(decimal, binario,otal y hexadecimal(PARTE 2).py
@@ -1,1 +1,11 @@
+# Base Octal
+a = int('27', 8)
+print(f"a = base octal: {a}")
 
+# Base Hexadecimal
+a = int('17', 16)
+print(f"a = base hexadecimal: {a}")
+
+# Base 5 (sus valores son de 0 a 4)
+a = int('344', 5)
+print(f"a = base 5: {a}")
