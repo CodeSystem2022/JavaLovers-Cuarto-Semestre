b18423aba4bd8b5dfa6ef6f5dbc91719dbfbe14e
Alexa Nahuel <112024900+alexanahuel@users.noreply.github.com>
2023-11-11 09:55:13 -0300

    Update ILibroServicio.java

--- Java/Semana 07/ILibroServicio.java
+++ Java/Semana 07/ILibroServicio.java
@@ -1,1 +1,9 @@
+package utn.tiendo_libros.servicio;
 
+import utn.tiendo_libros.modelo.Libro;
+import java.util.List;
+public interface ILibroServicio {
+    public List<Libro> listarLibros();
+    public Libro buscarLibroPorId(Integer idLibro);
+    public void eliminarLibro(Libro libro);
+}
