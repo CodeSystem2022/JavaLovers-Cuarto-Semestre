afe725bf1144e4063e0f4aa7a8ab4fbb333a47ec
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-11-11 09:47:38 -0300

    Update LibroRepositorio.java

--- Java/Semana 07/LibroRepositorio.java
+++ Java/Semana 07/LibroRepositorio.java
@@ -1,1 +1,7 @@
+package utn.tiendo_libros.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.tiendo_libros.modelo.Libro;
+public interface LibroRepositorio  extends JpaRepository<Libro Integer>{
+}
 
