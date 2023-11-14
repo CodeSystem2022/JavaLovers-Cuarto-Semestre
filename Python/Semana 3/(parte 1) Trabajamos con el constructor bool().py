5e17836cf4e934fa54b3a30f2b635dc128bb6ed2
Carrizo Andres <93688247+andrescarrizo1@users.noreply.github.com>
2023-11-13 21:11:56 -0300

    Create (parte 1) Trabajamos con el constructor bool().py

---
+++ Python/Semana 3/(parte 1) Trabajamos con el constructor bool().py
@@ -0,0 +1,9 @@
+#Bool contiene los valores de True y False
+#Los tipos numericos, es false para el 0 (cero), true para los demas valores
+valor = 0.0
+resultado = boll(valor)
+print(f'valor: {valor}, Resultado: {resultado}´)
+
+valor = 0.1
+resultado = bool (valor)
+print(f'valor:{valor}, Resultado: {resultado}')
