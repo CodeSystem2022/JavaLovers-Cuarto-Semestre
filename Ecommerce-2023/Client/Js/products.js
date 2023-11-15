81648c8668caea5ef1cb36ffc402285d10a3fe47
Aldo Rosales <112834554+Aldo1880@users.noreply.github.com>
2023-11-14 23:43:11 -0300

    Add files via upload

---
+++ Ecommerce-2023/Client/Js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+
+        img: "/client/media/banana.png",
+=======
+        img: "/cleint/media/banana.png",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/client/media/leche.png",
+    },
+    { "/client/media/pollo.png",
+
+    },
+    {
+        id: 3,
+        productName: "pollo",
+        price: 750,
+        qunty: 1,
+        img:
+    },
+    {
+        id: 4,
+        productName: "mayonesa",
+        price: 510,
+        qunty: 1,
+        img: "/client/media/mayonesa.png",
+
+    },
+]
